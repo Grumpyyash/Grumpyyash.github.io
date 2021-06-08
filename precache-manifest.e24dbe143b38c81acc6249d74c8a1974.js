0c57f78df2a2c7a26043f62321190cba20634e49
Yash Vardhan <yashvardhan513@gmail.com>
2021-06-08 20:03:28 +0530

    Updates

---
+++ precache-manifest.e24dbe143b38c81acc6249d74c8a1974.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec464c1b11a8661d34b45786471d48e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a9ea6b838a7cd61833",
+    "url": "/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "9fec42f96a486db60f90",
+    "url": "/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "58fbf8f33273d8ede718",
+    "url": "/static/css/main.d68614cc.chunk.css"
+  },
+  {
+    "revision": "4514e786a79b53413a35",
+    "url": "/static/js/2.21e0061b.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.21e0061b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a9ea6b838a7cd61833",
+    "url": "/static/js/3.7d26c151.chunk.js"
+  },
+  {
+    "revision": "9fec42f96a486db60f90",
+    "url": "/static/js/4.3e19ea36.chunk.js"
+  },
+  {
+    "revision": "58fbf8f33273d8ede718",
+    "url": "/static/js/main.dd2e654e.chunk.js"
+  },
+  {
+    "revision": "1658795f0c7b5497db07",
+    "url": "/static/js/runtime-main.554bf8a0.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "cde7daf2297b38128f4032d05efef7d4",
+    "url": "/static/media/acm-logo.cde7daf2.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8d8c584b857f474e61a6794b09e481d2",
+    "url": "/static/media/hack-logo.8d8c584b.jpg"
+  },
+  {
+    "revision": "644cc984efdd52064812955073272b50",
+    "url": "/static/media/ism-logo.644cc984.png"
+  },
+  {
+    "revision": "d226a9a456a4d3ce506744c183ca4fc1",
+    "url": "/static/media/layer-logo.d226a9a4.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "a3fcb02714c861d7e66a03680021cfc3",
+    "url": "/static/media/nighthawk-logo.a3fcb027.svg"
+  },
+  {
+    "revision": "a51184ef14766467d35e266c69387aa4",
+    "url": "/static/media/overflow-logo.a51184ef.jpeg"
+  },
+  {
+    "revision": "f7ff5c23ceb95f104a56061c48df14d5",
+    "url": "/static/media/score-logo.f7ff5c23.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
